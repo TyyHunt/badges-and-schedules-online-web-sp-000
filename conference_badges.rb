c5a61bca6a5ae13ee5b4508b38ae7c6b6238c932
Tyler Hunt <tyhunt04@gmail.com>
2019-02-16 19:38:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,11 +26,9 @@ end
 
 def printer(speakers)
   #print out the badges and room assignments individually
-  print_room = assign_rooms(speakers)
   assign_rooms(speakers).each do |message|
     puts message
   end
-  print_badge = batch_badge_creator(speakers)
   batch_badge_creator(speakers).each do |message|
     puts message
   end
